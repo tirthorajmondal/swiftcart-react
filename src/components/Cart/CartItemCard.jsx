688706df4748b515e911c27e79c7d437a1708cb8
tirthorajmondal <tirtho.raj.2003@gmail.com>
2026-02-20 00:50:54 +0600

    cart items displayed on cart page

---
+++ src/components/Cart/CartItemCard.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { RiDeleteBin6Fill } from "react-icons/ri";
+
+const CartItemCard = ({ product }) => {
+    const { id, title, image, category, price, } = product;
+    const [qty, setQty] = useState(1)
+    // const [total, setTotal] = useState(0)
+
+    const decreaseCount = () => {
+        if (qty === 1) return
+        setQty(qty - 1)
+    }
+
+
+    const deleteCartItem = (id) => {
+        console.log(id);
+    }
+    return (
+        <div className='flex  items-center gap-3 md:gap-6 bg-white p-3 md:p-5 rounded-2xl shadow-sm border border-gray-100'>
+            <div className="w-16 md:w-32 h-16 md:h-32 shrink-0 bg-gray-100 rounded-xl p-2">
+                <img src={image} alt={title} className="w-full h-full object-contain" />
+            </div>
+
+            <div className="grow">
+                <h3 className="md:text-lg font-bold text-slate-800 line-clamp-1 md:line-clamp-2">{title}</h3>
+                <p className="text-xs md:text-sm text-slate-500 uppercase mb-2">{category}</p>
+                <p className="md:text-xl font-black text-indigo-600">${price}</p>
+            </div>
+
+            <div className="flex items-center border border-gray-200 rounded-lg">
+                <button
+                    onClick={decreaseCount}
+
+                    className="px-3 py-1 bg-gray-50 hover:bg-gray-100 border-r border-gray-200">-</button>
+                <span className="px-4 py-1 font-bold">{qty}</span>
+                <button
+                    onClick={() => setQty(qty + 1)}
+                    className="px-3 py-1 bg-gray-50 hover:bg-gray-100 border-l border-gray-200">+</button>
+            </div>
+
+            <button onClick={() => deleteCartItem(id)} className="btn btn-ghost btn-circle text-red-400 hover:text-red-600">
+                <RiDeleteBin6Fill className="text-red-500 text-xl" />
+            </button>
+        </div>
+    );
+};
+
+export default CartItemCard;
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
+import CartItemCard from "../../components/Cart/CartItemCard";
+import { getCart } from "../../utils";
 
 const Cart = () => {
+    const [items, setItems] = useState(getCart() || [])
+    const [sutTotal, setSubTotal] = useState(0)
+
+
+    console.log(items);
+
     return (
-        <div>
-            Cart
+        <div className="container mx-auto p-6 min-h-screen ">
+            <h1 className="text-3xl font-black text-slate-800 mb-8">Your Shopping Bag</h1>
+
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+                <div className="lg:col-span-2 space-y-4 ">
+                    {/* cart items here */}
+                    {
+                        items.map(cart => <CartItemCard key={cart.id} product={cart} />)
+                    }
+                </div>
+
+                <div className="bg-white p-6 rounded-2xl shadow-sm border border-gray-100 h-fit sticky top-6">
+                    <h2 className="text-xl font-bold mb-4">Order Summary</h2>
+                    <div className="space-y-3 text-slate-600">
+                        <div className="flex justify-between">
+                            <span>Subtotal</span>
+                            <span >$0.00</span>
+                        </div>
+                        <div className="flex justify-between">
+                            <span>Shipping</span>
+                            <span className="text-green-600 font-medium">Free</span>
+                        </div>
+                        <div className="divider"></div>
+                        <div className="flex justify-between text-xl font-black text-slate-900">
+                            <span>Total</span>
+                            <span >$0.00</span>
+                        </div>
+                    </div>
+                    <button className="btn btn-primary w-full mt-6 text-white rounded-xl">
+                        Checkout Now
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
